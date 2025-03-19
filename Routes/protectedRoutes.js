f553e3e4af0efd7f4f21f27668750463e3e433f9
Wamiq <wamiqzahid319@gmail.com>
2025-03-19 19:22:31 +0500

    Fix import path casing for authMiddleware in protectedRoutes

--- Routes/protectedRoutes.js
+++ Routes/protectedRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
   sessionMiddleware,
   authorizeRoles,
-} from "../middlewares/authMiddleware.js";
+} from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
 
